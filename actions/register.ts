570431f62a5b7ce81174289bec25d625049134be
darshanrijal <darshanrijal0@gmail.com>
2024-06-29 09:24:45 +0545

    completed create user functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,10 +1,25 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
+import { GetUserByEmail } from "@/data/user";
 export const Register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFiedls = RegisterSchema.safeParse(values);
   if (!validatedFiedls.success) {
     return { error: "Invalid fields" };
   }
-  return { success: "Email sent" };
+  const { email, name, password } = validatedFiedls.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await GetUserByEmail(email);
+  if (existingUser) return { error: "Email already in use" };
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  //  TODO SEND VERIFICATION TOKEN EMAIL
+  return { success: "User created" };
 };
